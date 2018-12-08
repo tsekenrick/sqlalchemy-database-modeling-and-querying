d57c3d91ecb1b70ba5a05688758d69e083728910
tsekenrick <kt1556@nyu.edu>
2018-12-08 02:12:46 -0500

    finished part 1, going to write queries now.

--- mock.py
+++ mock.py
@@ -9,3 +9,130 @@ session = Session()
 
 # use the session object and imported classes (Company, Scooter, etc.)
 # ... to create companies, types and scooters in teh database
+
+################### Companies
+
+companies = []
+
+c1 = Company()
+c1.name = 'Fast Company'
+c1.website = 'fastco.com'
+c1.founded = 2000
+companies.append(c1)
+
+c2 = Company()
+c2.name = 'Generic Company'
+c2.website = 'genco.com'
+c2.founded = 2008
+companies.append(c2)
+
+c3 = Company()
+c3.name = 'Special Company'
+c3.website = 'vspecial.com'
+c3.founded = 2014
+companies.append(c3)
+
+################### Scooter Types
+
+scooter_types = []
+
+st1 = ScooterType()
+st1.model = 'FC Mk.1'
+st1.max_range = 300
+st1.weight = 30
+st1.max_speed = 50
+st1.manufacturer = c1 
+scooter_types.append(st1)
+
+st2 = ScooterType()
+st2.model = 'FC Lite'
+st2.max_range = 400
+st2.weight = 20
+st2.max_speed = 40 
+st2.manufacturer = c1
+scooter_types.append(st2)
+
+st3 = ScooterType()
+st3.model = 'FC Pro'
+st3.max_range = 500
+st3.weight = 50
+st3.max_speed = 70 
+st3.manufacturer = c1
+scooter_types.append(st3)
+
+###################
+
+st4 = ScooterType()
+st4.model = 'GC Generic'
+st4.max_range = 200
+st4.weight = 35
+st4.max_speed = 60 
+st4.manufacturer = c2
+scooter_types.append(st4)
+
+st5 = ScooterType()
+st5.model = 'GC Boring'
+st5.max_range = 250
+st5.weight = 40
+st5.max_speed = 55 
+st5.manufacturer = c2
+scooter_types.append(st5)
+
+st6 = ScooterType()
+st6.model = 'GC Coma Inducing'
+st6.max_range = 150
+st6.weight = 30
+st6.max_speed = 20 
+st6.manufacturer = c2
+scooter_types.append(st6)
+
+###################
+
+st7 = ScooterType()
+st7.model = 'SC Sonic'
+st7.max_range = 100
+st7.weight = 15
+st7.max_speed = 90
+st7.manufacturer = c3
+scooter_types.append(st7)
+
+st8 = ScooterType()
+st8.model = 'SC Juggernaut'
+st8.max_range = 600
+st8.weight = 100
+st8.max_speed = 50
+st8.manufacturer = c3
+scooter_types.append(st8)
+
+################### Scooters
+# set type, retired and acquire date
+scooters = []
+retired = [True, False, False, False] # creates roughly .25 chance retired
+
+for i in range(80):
+    tmp_scooter = Scooter()
+    
+    tmp_scooter.scooter_type = random.choice(scooter_types)
+    tmp_scooter.acquired_date = f'{random.randint(2014,2018)}-{random.randint(1, 12):02}-{random.randint(1,28):02}'
+    tmp_scooter.retired = random.choice(retired)
+    
+    scooters.append(tmp_scooter)
+    
+session.add_all(scooters)
+session.commit()
+    
+    
+
+# assuming session is created and all imports are present:
+companies = session.query(Company)
+for c in companies:
+    print(f'The company, {c}, has the following scooter models:');
+    for i, scooter_type in enumerate(c.scooter_types):
+        print(i + 1, scooter_type)
+    print('\n')
+ 
+scooter_types = session.query(ScooterType)
+for s in scooter_types:
+    print(f'{s.manufacturer.name} ==> {s.model}')
+ 
+ 
--- model.py
+++ model.py
@@ -10,10 +10,62 @@ from sqlalchemy.ext.declarative import declarative_base
 # call it Base!
 Base = declarative_base()
 
+
 # implement the following three classes...
+    
 
-# class Scooter(Base):
+    
+class ScooterType(Base):    
+    __tablename__ = 'scooter_type'
+    
+    scooter_type_id = Column('scooter_type_id', Integer, primary_key=True)
+    model = Column('model', String(50))
+    max_range = Column('max_range', Integer)
+    weight = Column('weight', Integer)
+    max_speed = Column('max_speed', Integer)
+    
+    company_id = Column('company_id', Integer, ForeignKey('company.company_id'))
+    manufacturer = relationship('Company', back_populates = 'scooter_types')
+    
+    # ???
+    scooters = relationship('Scooter', back_populates = 'scooter_type')
+    
+    def __str__(self):
+        return f'Company Name: {self.manufacturer.name}\nModel {self.model}\nMax Speed {self.max_speed}\nWeight {self.weight}'
+        # return f'{self.manufacturer} {self.model}: max speed is {self.max_speed}'
+    def __repr__(self):
+        return self.__str__()
 
-# class ScooterType(Base):
+    
+class Company(Base):
+    __tablename__ = 'company'
+    
+    company_id = Column('company_id', Integer, primary_key=True)
+    name = Column('name', String(50))
+    website = Column('website', String(50))
+    founded = Column('founded', Integer)
+    
+    scooter_types = relationship(ScooterType, back_populates='manufacturer')
 
-# class Company(Base):
+    def __str__(self):
+        return f'{self.name} - Website: {self.website} - Est. {self.founded}'
+    
+    def __repr__(self):
+        return self.__str__()
+
+class Scooter(Base):
+    __tablename__ = 'scooter'
+
+    scooter_id = Column('scooter_id', Integer, primary_key=True)    
+    acquired_date = Column('acquired_date', Date)
+    retired = Column('retired', Boolean, default=False)
+    
+    # one-to-many for scooter type to scooters
+    scooter_type_id = Column('scooter_type_id', Integer, ForeignKey('scooter_type.scooter_type_id'))
+    scooter_type = relationship('ScooterType', back_populates = 'scooters')
+    
+    def __str__(self):
+        return f'Scooter #{self.scooter_id}\nRetired? {self.retired}\nScooter Type:\n{self.scooter_type}'
+
+    def __repr__(self):
+        return self.__str__()
