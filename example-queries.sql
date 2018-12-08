1835b85153a8462ebf2fc5cf39e05deef03d119d
tsekenrick <kt1556@nyu.edu>
2018-12-08 02:30:32 -0500

    finished part 2

--- example-queries.sql
+++ example-queries.sql
@@ -1,11 +1,11 @@
 -- 1.
-
+select scooter_id, name, founded, model, weight, max_speed, acquired_date, retired
+  from scooter
+  inner join scooter_type on scooter.scooter_type_id = scooter_type.scooter_type_id
+  inner join company on scooter_type.company_id = company.company_id limit 10;
 -- 2.
-
+select retired, count(*) from scooter group by retired;
 -- 3.
-
+select to_char(acquired_date, 'YYYY-MM'), count(*) from scooter group by to_char(acquired_date, 'YYYY-MM');
 -- 4. 
-
--- 5.
-
--- 6.
+select company_id, count(*) from scooter_type group by company_id;
